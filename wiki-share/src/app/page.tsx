a9e55a7f7b5fd25724aecaf81cff6bf3c377d7d1
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-11-08 15:32:53 +0100

    add pages, install icons library

--- wiki-share/src/app/page.tsx
+++ wiki-share/src/app/page.tsx
@@ -1,8 +1,15 @@
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1> Hello World ! </h1>
-      <p> My name is WikiShare </p>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1> Hello World ! </h1>
+        <p> My name is WikiShare </p>
+      </main>
+      <Footer />
+    </>
   );
 }
